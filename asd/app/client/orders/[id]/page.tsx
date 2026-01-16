96ec63df61634d9743d01f15d1e38ee8525c2124
kalamkaskursabayeva <67894247+kalamkaskursabayeva@users.noreply.github.com>
2026-01-17 02:13:55 +0500

    Update page.tsx

--- asd/app/client/orders/[id]/page.tsx
+++ asd/app/client/orders/[id]/page.tsx
@@ -27,7 +27,7 @@ export default function OrderDetailsPage({ params }: { params: Promise<{ id: str
 
     const fetchOrder = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/orders/${orderId}`)
+        const response = await fetch(`https://sultansflowers-production.up.railway.app/orders/${orderId}`)
         const data = await response.json()
         
         if (data.success) {
